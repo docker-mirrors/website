10b6f4f650e4d06a55dbd05b280ab608b9dc8e9d
innocces <18435105624@163.com>
2024-06-15 22:57:40 +0800

    feat(fetch): image search list apis

---
+++ lib/images.ts
@@ -0,0 +1,50 @@
+import { stringify } from 'qs'
+import merge from 'lodash.merge'
+import type { ImageType, ImageDetail } from '@/types/image'
+
+export function makeRequestSource(source: string) {
+  return [process.env.DOCK_HUB_HOST, source].join('/')
+}
+
+export async function request<T = any>(sources: string, init?: RequestInit) {
+  const url = makeRequestSource(sources)
+  try {
+    const serverResponse = await fetch(
+      url,
+      merge(
+        {
+          headers: {
+            'user-agent': 'PostmanRuntime/7.37.0'
+          }
+        } as RequestInit,
+        init
+      )
+    )
+    const jsonResponse = await serverResponse.json()
+    return jsonResponse as T
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export function queryImages(
+  tag: string,
+  type: ImageType,
+  payload?: {
+    size: number
+    from: number
+    official?: boolean
+    open_source?: boolean
+  }
+) {
+  const query = {
+    size: 4,
+    from: 0,
+    ...payload,
+    source: type,
+    query: tag
+  }
+  return request<{ total: number; results: ImageDetail[] }>(
+    `api/search/v3/catalog/search?${stringify(query)}`
+  )
+}
---
+++ types/image.ts
@@ -0,0 +1,61 @@
+export enum SYSTEM {
+  LINUX = 'linux',
+  UNKNOWN = 'unknown',
+  WINDOWS = 'windows'
+}
+
+export type OperatingSystem = {
+  name: SYSTEM
+  label: Uppercase<SYSTEM>
+}
+
+export type Repository = {
+  name: string
+  namespace: string
+  description: string
+  type: ImageType
+  pull_count: string
+  is_automated: boolean
+  is_official: boolean
+  is_trusted: boolean
+  last_pushed_at: string
+  last_pulled_at: string
+}
+
+export type Architecture = {
+  name: string
+  label: string
+}
+
+export type RatePlanItem = {
+  id: string
+  repositories: Repository[]
+  operating_systems: OperatingSystem[]
+  architectures: Architecture[]
+}
+
+export type CategoryItem = {
+  slug: string
+  name: string
+}
+
+export type ImageType = 'store' | 'community' | 'image'
+
+export type ImageDetail = {
+  id: string
+  name: string
+  slug: string
+  type: ImageType
+  created_at: string
+  updated_at: string
+  short_description: string
+  source: ImageType
+  star_count: number
+  extension_reviewed: boolean
+  logo_url: {
+    large: string
+    small: string
+  }
+  categories: CategoryItem[]
+  rate_plans: RatePlanItem[]
+}
