448ce877affe32777d3a8d5d0ad7f996e51b13be
innocces <18435105624@163.com>
2024-06-16 20:24:01 +0800

    feat(logo): approve logo image component

---
+++ components/Logo.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import { useMemo } from 'react'
+
+export type LogoProps = {
+  src?: string
+  size?: 24 | 36
+}
+
+export function Logo({ src = '/docker-reply.svg', size = 24 }: LogoProps) {
+  const imageProps = useMemo(() => {
+    if (src.startsWith('http')) {
+      const source = src.replace(new URL(src).origin, '')
+      return {
+        src: source,
+        loader: () => {
+          return src
+        }
+      }
+    }
+
+    return {
+      src
+    }
+  }, [src])
+  return (
+    <Image
+      priority
+      {...imageProps}
+      width={size}
+      height={size}
+      alt={imageProps.src}
+    />
+  )
+}
