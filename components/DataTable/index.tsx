b0a196a00c533fbf05a6b41bcec2c45ebd0f6a8e
TinsFox <fox@tinsfox.com>
2024-06-17 13:16:11 +0800

    feat: max width

--- components/DataTable/index.tsx
+++ components/DataTable/index.tsx
@@ -86,7 +86,7 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 max-w-fit mx-auto">
       <div className="rounded-md border">
         <Table>
           <TableHeader>
