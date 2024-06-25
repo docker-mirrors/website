1ac1da44b2a4cf4bb8967c54e19657667aa55c9e
innocces <jiangjiangjiangxi@gmail.com>
2024-06-25 15:05:35 +0800

    docs(hljs): todolist plugin add

--- lib/hl.ts
+++ lib/hl.ts
@@ -8,13 +8,14 @@ export const md = MarkdownIt({
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return hljs.highlight(str, { language: lang }).value;
+        return hljs.highlight(str, { language: lang, ignoreIllegals: true })
+          .value;
       } catch (__) {}
     }
 
     return ''; // use external default escaping
   },
-});
+}).use(require('markdown-it-task-lists'));
 
 export async function hl(code: string) {
   return md.render(code);
